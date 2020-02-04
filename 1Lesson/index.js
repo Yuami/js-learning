3fe584dc4492ccc6605a79e0b944ca4bee2073aa
Nieves <nievescg98@gmail.com>
2020-02-04 22:18:34 +0100

    learning basic concepts

---
+++ 1Lesson/index.js
@@ -0,0 +1,4 @@
+const math = require("./math.js");
+
+console.log(math.sum(1,2));
+console.log(math.res(1,2));
---
+++ 1Lesson/math.js
@@ -0,0 +1,10 @@
+function sum (a,b){
+ return a+b;
+}
+function res (a,b){
+    return a-b;
+   }
+Mats= {};
+Mats.sum=sum;
+Mats.res=res;
+module.exports = Mats;
